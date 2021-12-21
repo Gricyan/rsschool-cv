66557fbcc937b908cf71bb5e861485b22a0b17e5
Ivan Gricyan <gricyan82@gmail.com>
2021-12-21 22:53:43 +0100

    feat: add script for menu button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// js code will be here
+$(document).ready(function() {
+    $('.header_burger, .header_list').click(function(event) {
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
